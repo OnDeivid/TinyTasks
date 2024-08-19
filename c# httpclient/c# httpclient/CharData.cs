eee5113a08acf0765615467fe681e7adae233327
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-08-19 20:52:52 +0300

    C#FirstRequest

---
+++ c# httpclient/c# httpclient/CharData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace c__httpclient
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Species { get; set; }
+        public string Type { get; set; }
+        public string Gender { get; set; }
+        public string Image { get; set; }
+        public List<string> Episode { get; set; }
+    }
+
+    public class CharacterListResponse
+    {
+        public List<Character> Results { get; set; }
+    }
+}
---
+++ c# httpclient/c# httpclient/Program.cs
@@ -0,0 +1,51 @@
+﻿using c__httpclient;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace RickAndMortyClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            string url = "https://rickandmortyapi.com/api/character";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                string jsonString = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonString);
+
+                var characterListResponse = JsonSerializer.Deserialize<CharacterListResponse>(jsonString, new JsonSerializerOptions
+                {
+                    //game changer !!!
+                    PropertyNameCaseInsensitive = true
+                });
+
+                if (characterListResponse?.Results != null)
+                {
+                    foreach (var character in characterListResponse.Results)
+                    {
+                        Console.WriteLine($"Name: {character.Name}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No characters");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"error: {e.Message}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
