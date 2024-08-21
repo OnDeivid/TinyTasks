8198a8c26cb8c28571841674d9f9aadaadebc9ed
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-08-21 21:01:36 +0300

    project to help me recall I/O

---
+++ RecallFlow IO/RecallFlow IO/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecallFlow_IO
+{
+    public partial class Form1 : Form
+    {
+        private List<string> lines;
+        private string TextPath;
+        int count = -1;
+        bool lineByline = true;
+        public Form1()
+        {
+            InitializeComponent();
+            lines = new List<string>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+
+            openfile.Filter = "Text Files (*.txt)|*.txt";
+            openfile.Title = "Select text file";
+
+            if (openfile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TextPath = openfile.FileName;
+            }
+
+            StreamReader reader = new StreamReader(TextPath);
+
+               textBox1.Text = reader.ReadToEnd();
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lineByline = true;
+        }
+
+    }
+}
