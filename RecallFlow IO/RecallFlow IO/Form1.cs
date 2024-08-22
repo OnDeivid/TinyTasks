e5462bdf8e1a14d751ce94ccfe85a824ac6455f2
onDeivid <141053918+OnDeivid@users.noreply.github.com>
2024-08-22 17:21:03 +0300

    Revert "simple"
    This reverts commit 769e877a0b0a3c7988d4f5564dd2d8f0c7fd2cc1.

--- RecallFlow IO/RecallFlow IO/Form1.cs
+++ RecallFlow IO/RecallFlow IO/Form1.cs
@@ -13,16 +13,20 @@ namespace RecallFlow_IO
 {
     public partial class Form1 : Form
     {
+        private List<string> lines;
         private string TextPath;
+        int count = -1;
+        bool lineByline = true;
         public Form1()
         {
             InitializeComponent();
+            lines = new List<string>();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             OpenFileDialog openfile = new OpenFileDialog();
+
             openfile.Filter = "Text Files (*.txt)|*.txt";
             openfile.Title = "Select text file";
 
@@ -31,29 +35,17 @@ namespace RecallFlow_IO
                 TextPath = openfile.FileName;
             }
 
-
             StreamReader reader = new StreamReader(TextPath);
-            textBox1.Text = reader.ReadToEnd();
-            reader.Close();
+
+               textBox1.Text = reader.ReadToEnd();
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+
+        private void button2_Click(object sender, EventArgs e)
         {
-
+            lineByline = true;
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            StreamWriter writer = new StreamWriter(TextPath);
-
-            writer.WriteLine(textBox1.Text);
-            writer.Close();
-        }
     }
 }
